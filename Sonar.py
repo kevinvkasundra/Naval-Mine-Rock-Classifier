3c6eb9a262f7ebb3f6f60402a8b430d7e57ec8f2
Kevin Vinay Kasundra <46608418+kevinvkasundra@users.noreply.github.com>
2019-04-29 17:15:26 -0500

    Commit 2

--- Sonar.py
+++ Sonar.py
@@ -1,14 +1,12 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+
 #file import
 path = ""
 df = pd.read_csv(path + "sonar_hw1.csv")
 df.head()
-y = []
-#dropping the target value
-y = df['object']
-df = df.drop('object', axis=1)
 
+##### Outlier Analysis #####
 
 #Number of data points
 n = df.shape[0] 
@@ -38,24 +36,83 @@ for i in df_list:
     dataframe = dataframe.join(i)
 print(dataframe)
 
+##### Model fitting #####
 
-X = df
+X = df.drop(['R41','R46'], axis =1) #Dropping the whole Columns
+D = X.dropna() #Dropping the missing value rows 
+y = D['object']
+X = D.drop('object', axis=1) #dropping the target value
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
                                                     random_state=4)
+
 print ('Train set:', X_train.shape, y_train.shape)
 print ('Test set:', X_test.shape, y_test.shape)
-###############################################################################
-from sklearn import svm
+
+# Logistic Regression #
 from sklearn.linear_model import LogisticRegression
-from sklearn.datasets import load_iris
 
-clf = svm.SVC(kernel='rbf')
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+y_pred = logreg.predict(X_test)
+print('Accuracy of logistic regression classifier on test set: {:.2f}'.
+      format(logreg.score(X_test, y_test)))
+#Confusion Matrix
+from sklearn.metrics import confusion_matrix
+confusion_matrix = confusion_matrix(y_test, y_pred)
+print(confusion_matrix)
+#Precision, Recall, F score
+from sklearn.metrics import classification_report
+print(classification_report(y_test, y_pred))
+#ROC Curve
+import matplotlib.pyplot as plt 
+plt.rc("font", size=14)
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+
+logit_roc_auc = roc_auc_score(y_test, logreg.predict(X_test))
+fpr, tpr, thresholds = roc_curve(y_test, logreg.predict_proba(X_test)[:,1])
+plt.figure()
+plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC for Logistic Regression')
+plt.legend(loc="lower right")
+plt.savefig('Log_ROC')
+plt.show()
+
+# SVM #
+from sklearn import svm
+
+clf= svm.SVC(kernel='linear',C = 1.0,probability=True, random_state =12345)
 clf.fit(X_train, y_train)
-
-X, y = load_iris(return_X_y = True)
-clf = LogisticRegression(random_state = 123, solver = 'lbfgs', 
-                         multi_class ='multinomial', max_iter = 1000).fit(X,y)
-
-clf.predict(df[:2,:])
-clf.predict_proba(df[:2, :])
-clf.score(X,y)
+y_pred = clf.predict(X_test)
+print('Accuracy of SVM classifier on test set: {:.2f}'.
+      format(clf.score(X_test, y_test)))
+#Confusion Matrix
+from sklearn.metrics import confusion_matrix
+confusion_matrix = confusion_matrix(y_test, y_pred)
+print(confusion_matrix)
+#Precision, Recall, F score
+print(classification_report(y_test, y_pred))
+#ROC Curve
+import matplotlib.pyplot as plt 
+plt.rc("font", size=14)
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+logit_roc_auc = roc_auc_score(y_test, clf.predict(X_test))
+fpr, tpr, thresholds = roc_curve(y_test, clf.predict_proba(X_test)[:,1])
+plt.figure()
+plt.plot(fpr, tpr, label='SVM (area = %0.2f)' % logit_roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC for SVM')
+plt.legend(loc="lower right")
+plt.savefig('Log1_ROC')
+plt.show()
